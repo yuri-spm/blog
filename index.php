bfa6907cd768f4e0bb09274eabf17c24bd977a99
Yuri do Monte <yspm.developer@gmail.com>
2024-02-13 11:03:39 -0300

    class 31

--- index.php
+++ index.php
@@ -4,16 +4,4 @@ require_once "sistema/configuracao.php";
 include_once "Helpers.php";
 
 
-
-// if(validarEmail('teste@gmailcom')){
-//     echo "Endereço de e-mail Valido";
-// }else{
-//     echo "E-mail invalido";
-// }
-
-
-if(validarUrl('TESTE')){
-    echo "Endereço valido";
-}else{
-    echo "Endereço invalido";
-}
+echo SITE_DESCRICAO;
