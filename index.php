2a50de55e9960792a257f8d8147ac8ca925d0edd
Yuri do Monte <yspm.developer@gmail.com>
2024-02-16 13:04:54 -0300

    class 44

--- index.php
+++ index.php
@@ -4,7 +4,6 @@ use system\Core\Message;
 
 require_once "system/config.php";
 include_once "Helpers.php";
-include './system/Core/Message.php';
 
 echo'<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">';
 
