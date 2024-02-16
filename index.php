ac8a0f54cb06793b53181ecc34a67076c005e900
Yuri do Monte <yspm.developer@gmail.com>
2024-02-16 13:35:31 -0300

    class 47

--- index.php
+++ index.php
@@ -2,19 +2,19 @@
 
 use system\Core\Helpers;
 use system\Core\Message;
+use system\Core\Controller;
+
 
 require_once "system/config.php";
 include_once "./system/Core/Helpers.php";
 include './system/Core/Message.php';
+include './system/Core/Controller.php';
 
 echo'<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">';
 
-echo (new Message)->success('Aula 43');
-
+$controller = new Controller();
 echo '<hr>';
-
-
-var_dump(Helpers::validateCPF('14594140718'));
+var_dump($controller);
 
 
 
