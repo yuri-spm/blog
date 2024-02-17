dafcb777d49367900aec9deb63968f125624b98f
Yuri do Monte <yspm.developer@gmail.com>
2024-02-16 21:58:47 -0300

    class 51

--- index.php
+++ index.php
@@ -8,13 +8,8 @@ require 'vendor/autoload.php';
 
 echo'<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">';
 
-$controller = new Controller();
-echo '<hr>';
-var_dump($controller);
 
-echo Helpers::greetings();
 
-echo SITE_NOME;
 
 
 
