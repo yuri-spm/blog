56ca35aa8a6a7dec73db5d8676700c2ddf721e2f
Yuri do Monte <yspm.developer@gmail.com>
2024-02-22 12:50:34 -0300

    class 75.5

---
+++ system/Model/CategoryModel.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace system\Model;
+
+use system\Core\Connect;
+
+class CategoryModel
+{
+
+    protected $query;
+
+    /** @var string */
+    protected $params;
+
+    /** @var string */
+    protected $order;
+
+    /** @var int */
+    protected $limit;
+
+    /** @var int */
+    protected $offset;
+
+    public function find($columns = '*'): array
+    {   
+       $query = "SELECT {$columns} FROM category";
+       $stmt = Connect::getInstance()->query($query);
+       $result = $stmt->fetchAll();
+
+        return $result;
+    }
+
+    public function findByID(int $id, $columns = '*'): bool|object
+    {   
+       $query = "SELECT {$columns} FROM category WHERE id = {$id}";
+       $stmt = Connect::getInstance()->query($query);
+       $result = $stmt->fetch();
+       return $result;
+    }
+
+
+}
