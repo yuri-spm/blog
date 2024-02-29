73dbd5e964c714027a7e4aa27d000c82a200cbdc
Yuri do Monte <yspm.developer@gmail.com>
2024-02-28 22:09:35 -0300

    class 78

--- templates/site/assets/js/site.js
+++ templates/site/assets/js/site.js
@@ -1,1 +1,19 @@
-
+$(document).ready(function(){
+    $("#search").keyup(function(){
+        var search = $(this).val();
+        if(search !== ""){
+            $.ajax({
+                url: $('form').attr('data-url-search'),
+                method: 'POST',
+                data: {
+                    search: search
+                },
+                sucess: function(data){
+                    $('#request').html(data);
+                }
+            });
+        }else{
+            $('#request').css('display', 'none')
+        }
+    });
+});
