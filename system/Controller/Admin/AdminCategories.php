e13d56406db4858c7962961c9d28abd3407f9b01
Yuri do Monte <yspm.developer@gmail.com>
2024-04-05 15:08:45 -0300

    change AdminCategories

--- system/Controller/Admin/AdminCategories.php
+++ system/Controller/Admin/AdminCategories.php
@@ -20,6 +20,7 @@ class AdminCategories extends AdminController
 
     public function register()
     {
+
         $data = filter_input_array(INPUT_POST, FILTER_DEFAULT);
         if (isset($data)) {
             (new CategoryModel())->register($data);
@@ -29,16 +30,43 @@ class AdminCategories extends AdminController
         echo $this->template->render('categories/forms_categories.html.twig', []);
     }
 
+    // public function edit(int $id):void
+    // {
+       
+    //     $category = (new CategoryModel())->findByID($id);
+    //     var_dump($category);
+    //     die();
+    //     $data = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+      
+    //     if(isset($data)){
+    //         var_dump($data);
+    //         die();
+    //          (new CategoryModel())->update($data, $id);
+    //          Helpers::redirect('admin/categories/forms_categories.html.twig');
+    //     }
+        
+    //     echo $this->template->render('categories/forms_categories.html.twig', [
+    //         'categories' => $category
+    //     ]);
+    // }
+
     public function edit($id)
     {
-        $category = (new CategoryModel())->findByID($id);
+        $category = (new CategoryModel())->findByID($id); 
 
+        $data = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+       
+        if(isset($data)){
+            (new CategoryModel())->update($data, $id);
+            Helpers::redirect('admin/categories/categories'); 
+        }
         echo $this->template->render(
-            'categories/forms_categories.html.twig',
-            [
-                'categories'    => $category
-
-            ]
-        );
+                'categories/forms_categories.html.twig',
+                [
+                    
+                    'categories'    => $category            
+                ]
+            );
     }
 }
+
