af30ef2c101952db6308c26d3bfda9c0414a36c3
Yuri do Monte <yspm.developer@gmail.com>
2024-04-09 09:23:49 -0300

    add function delete

--- system/Model/PostModel.php
+++ system/Model/PostModel.php
@@ -9,91 +9,94 @@ use system\Core\Message;
 class PostModel
 {
 
-    protected $query;
+   protected $query;
 
-    /** @var string */
-    protected $params;
+   /** @var string */
+   protected $params;
 
-    /** @var string */
-    protected $order;
+   /** @var string */
+   protected $order;
 
-    /** @var int */
-    protected $limit;
+   /** @var int */
+   protected $limit;
 
-    /** @var int */
-    protected $offset;
+   /** @var int */
+   protected $offset;
 
-    public function find(): array
-    {   
-       $query = "SELECT * FROM posts WHERE status = 1 ";
-       $stmt = Connect::getInstance()->query($query);
-       $result = $stmt->fetchAll();
-
-        return $result;
-    }
-
-    public function findByID(int $id): bool|object
-    {   
-       $query = "SELECT * FROM posts WHERE id = {$id}";
-       $stmt = Connect::getInstance()->query($query);
-       $result = $stmt->fetch();
-       return $result;
-    }
-
-    public function all(): array
-    {   
-       $query = "SELECT * FROM posts";
-       $stmt = Connect::getInstance()->query($query);
-       $result = $stmt->fetchAll();
-
-        return $result;
-    }
-
-    public function search($search): array
-    {   
-       $query = "SELECT* FROM posts WHERE status = 1 AND title LIKE '%{$search}%'";
-       $stmt = Connect::getInstance()->query($query);
-       $result = $stmt->fetchAll();
-
-        return $result;
-    }
-
-    public function register(array $data)
-    {
-       try {
- 
-          $query = 'INSERT INTO posts(`category_id`,`title`, `text`, `status`) VALUES (?,?,?,?)';
-          $stmt = Connect::getInstance()->prepare($query);
-          $stmt->execute(
-             [
-                $data[ 'category_id'],
-                $data[ 'title'],
-                $data['text'],
-                $data['status']
-             ]);
-       } catch (PDOException $e) {
-          echo (new Message())->error($e);
-       }
-    }
-
-    public function update(array $data, int $id):void
+   public function find(): array
    {
-      var_dump($data);
-      die();
-       try {
-          $query = "UPDATE posts SET category_id = :category_id, title = :title, text = :text, status = :status WHERE id = {$id}";
-          $stmt = Connect::getInstance()->prepare($query);
-          $stmt->execute( [
-            $data[ 'category_id'],
-            $data[ 'title'],
+      $query = "SELECT * FROM posts WHERE status = 1 ";
+      $stmt = Connect::getInstance()->query($query);
+      $result = $stmt->fetchAll();
+
+      return $result;
+   }
+
+   public function findByID(int $id): bool|object
+   {
+      $query = "SELECT * FROM posts WHERE id = {$id}";
+      $stmt = Connect::getInstance()->query($query);
+      $result = $stmt->fetch();
+      return $result;
+   }
+
+   public function all(): array
+   {
+      $query = "SELECT * FROM posts";
+      $stmt = Connect::getInstance()->query($query);
+      $result = $stmt->fetchAll();
+
+      return $result;
+   }
+
+   public function search($search): array
+   {
+      $query = "SELECT* FROM posts WHERE status = 1 AND title LIKE '%{$search}%'";
+      $stmt = Connect::getInstance()->query($query);
+      $result = $stmt->fetchAll();
+
+      return $result;
+   }
+
+   public function register(array $data)
+   {
+      try {
+
+         $query = 'INSERT INTO posts(`category_id`,`title`, `text`, `status`) VALUES (?,?,?,?)';
+         $stmt = Connect::getInstance()->prepare($query);
+         $stmt->execute(
+            [
+               $data['category_id'],
+               $data['title'],
+               $data['text'],
+               $data['status']
+            ]
+         );
+      } catch (PDOException $e) {
+         echo (new Message())->error($e);
+      }
+   }
+
+   public function update(array $data, int $id): void
+   {
+      try {
+         $query = "UPDATE posts SET category_id = :category_id, title = :title, text = :text, status = :status WHERE id = {$id}";
+         $stmt = Connect::getInstance()->prepare($query);
+         $stmt->execute([
+            $data['category_id'],
+            $data['title'],
             $data['text'],
             $data['status']
          ]);
-       } catch (PDOException $e) {
-          echo (new Message())->error($e);
-       }
-    }
+      } catch (PDOException $e) {
+         echo (new Message())->error($e);
+      }
+   }
 
-
-
+   public function delete(int $id)
+   {
+      $query = "DELETE FROM posts WHERE `posts`.`id` = {$id}";
+      $stmt = Connect::getInstance()->query($query);
+      $stmt->execute();
+   }
 }
