e40647e815ff661c6d1910ce096ed216b37f062f
Yuri do Monte <yspm.developer@gmail.com>
2024-04-23 23:11:31 -0300

    change index

--- index.php
+++ index.php
@@ -3,15 +3,25 @@
 //Arquivo index responsável pela inicialização do sistema
 
 use system\Core\Connect;
+use system\Core\Session;
 use system\Model\PostModel;
 
 require 'vendor/autoload.php';
 
 require 'routers.php';
 
-// $posts = (new PostModel())->find(2);
+// $session = new Session();
 
-// foreach($posts as $post){
-//     var_dump($post);
-// }
+// $session->create('user', ['id' => 10, 'nome' => 'Yuri do Monte']);
 
+// var_dump($session->load());
+
+// var_dump($session->check('user'));
+
+// // var_dump($session->clean('user'));
+
+// // var_dump($session->check('user'));
+
+// var_dump($session->deleted());
+
+// var_dump($session->check('nome'));
