a77625a278cb957a210dd3815fa83fe6f573d3a3
Yuri do Monte <yspm.developer@gmail.com>
2024-05-08 21:24:24 -0300

    add function register and filter

--- system/Core/Model.php
+++ system/Core/Model.php
@@ -15,6 +15,7 @@ class Model
     protected $order;
     protected $limit;
     protected $offset;
+    protected $filter;
 
 
     public function __construct(string $table)
@@ -71,7 +72,33 @@ class Model
             $this->erro = $e;
         }
     }
-    
+
+    protected function register(array $data)
+    {
+       try{
+            $columns = implode(', ', array_keys($data));
+            $values  = ':' . implode(', :', array_keys($data));
+            
+            $query = "INSERT INTO " .$this->table . " {{$columns}} VALUES {{$values}}"; 
+            $stmt = Connect::getInstance()->prepare($query);
+            $stmt->execute($this->filter($data));
+
+            return Connect::getInstance()->lastInsertId();
+
+       }catch(PDOException $e){
+          echo $this->erro = $e;
+          return null;
+       }
+    }
+
+    private function filter(array $data)
+    {
+        $filter = [];
+
+        foreach($data as $key => $value){
+            $filter[$key] = (is_null($value) ? null : filter_var($value, FILTER_DEFAULT));
+        }
+    }
         
    
 }
