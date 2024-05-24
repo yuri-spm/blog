a3216a1cbfe5017663b74f71ab40d24f05c0dce5
Yuri do Monte <yspm.developer@gmail.com>
2024-05-24 13:57:47 -0300

    created validate login

--- system/Controller/Admin/AdminLogin.php
+++ system/Controller/Admin/AdminLogin.php
@@ -15,6 +15,23 @@ class AdminLogin extends Controller
     
     public function login(): void
     {
+      $data = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+      if(isset($data)){
+        if($this->checkData($data)){
+          $this->message->success('dados validados')->flash();
+        }
+      }
       echo $this->template->render('login.html.twig', []); 
+    }
+    
+    private function checkData(array $data): bool
+    {
+      if(isset($data)){
+        if(in_array('', $data)){
+          $this->message->alert('Todos os campos são obrigatorios')->flash();
+          return false;
+        }
+      }
+      return true;
     }
 }
