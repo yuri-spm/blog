d5502149fc532db4baa5d3502dd74caa47cf8bbf
Yuri do Monte <yspm.developer@gmail.com>
2024-06-09 10:43:48 -0300

    change adminuser

--- system/Controller/Admin/AdminUser.php
+++ system/Controller/Admin/AdminUser.php
@@ -7,117 +7,106 @@ use system\Core\Helpers;
 use system\Model\UserModel;
 
 class AdminUser extends AdminController
-{    
+{
+
+    
     /**
      * lists
      *
      * @return void
      */
-    public function lists()
+    public function lists(): void
     {
-        $users = new UserModel();
-        echo $this->template->render('users/users.html.twig', [
-            'users' => $users->find()->order('level DESC, status ASC')->result(true),
-            'total' => [
-                'users'         => $users->find('status = 1 AND level != 3')->count(),
-                'userActive'    => $users->find('status = 1 AND level != 3')->count(),
-                'userInactive'  => $users->find('status = 0 AND level != 3')->count(),
-                'admin'         => $users->find('level = 3')->count(),
-                'adminActive'   => $users->find('status = 1 AND level = 3')->count(),
-                'adminInactive' => $users->find('status = 0 AND level = 3')->count()
+        $user = new UserModel();
 
-            ]    
+        echo $this->template->render('usuarios/listar.html', [
+            'usuarios' => $user->find()->order('level DESC, status ASC')->result(true),
+            'total' => [
+                'usuarios' => $user->find('level != 3')->count(),
+                'usuariosAtivo' => $user->find('status = 1 AND level != 3')->count(),
+                'usuariosInativo' => $user->find('status = 0 AND level != 3')->count(),
+                'admin' => $user->find('level = 3')->count(),
+                'adminAtivo' => $user->find('status = 1 AND level = 3')->count(),
+                'adminInativo' => $user->find('status = 0 AND level = 3')->count()
+            ]
         ]);
     }
 
     
     /**
-     * register user
+     * register
      *
      * @return void
      */
-    public function register()
+    public function register(): void
     {
         $data = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+        if (isset($data)) {
+            //checa os data 
+            if ($this->validateData($data)) {
 
-        if(isset($data)){
-            if($this->validateData($data)){
-                if(empty($data['password'])){
+                if (empty($data['senha'])) {
                     $this->message->alert('Informe uma senha para o usuário')->flash();
-                }else{
+                } else {
                     $user = new UserModel();
 
-                    $user->name     = $data['name'];
-                    $user->email    = $data['email'];
-                    $user->password = $data['password'];
-                    $user->level    = $data['level'];
-                    $user->status   = $data['status'];
+                    $user->nome = $data['nome'];
+                    $user->email = $data['email'];
+                    $user->senha = $data['senha'];
+                    $user->level = $data['level'];
+                    $user->status = $data['status'];
 
-                    if($user->save()){
-                        $this->message->success('Usuário cadastrado com sucesso')->flash();
-                        Helpers::redirect('admin/users/users');
-                    }else{
-                        $user->message()->flash();
+                    if ($user->save()) {
+                         $this->message->success('Usuário cadastrado com sucesso')->flash();
+                          Helpers::redirect('admin/users/users');
+                    } else {
+                        $user->mensagem()->flash();
                     }
                 }
             }
         }
 
-        echo $this->template->render('users/forms_users.html.twig',[
-            'user' => $data
+        echo $this->template->render('usuarios/formulario.html', [
+            'usuario' => $data
         ]);
-        
     }
-
-    public function edit(int $id)
+    
+    /**
+     * editar
+     *
+     * @param  mixed $id
+     * @return void
+     */
+    public function editar(int $id): void
     {
         $user = (new UserModel())->findByID($id);
 
         $data = filter_input_array(INPUT_POST, FILTER_DEFAULT);
-
-        if(isset($data)){
-            if($this->validateData($data)){
+        if (isset($data)) {
+            if ($this->validateData($data)) {
                 $user = (new UserModel())->findByID($id);
 
-                $user->name      = $data['name'];
-                $user->email     = $data['email'];
-                $user->password  = (!empty($data['password']) ? $data['password'] : $user->password);
-                $user->level     = $data['level'];
-                $user->status    = $data['status'];
-                $user->update_at =  date('Y-m-d H:i:s');
+                $user->nome = $data['nome'];
+                $user->email = $data['email'];
+                $user->senha = (!empty($data['senha']) ? $data['senha'] : $user->senha);
+                $user->level = $data['level'];
+                $user->status = $data['status'];
+                $user->atualizado_em = date('Y-m-d H:i:s');
 
-                if($user->save()){
-                    $this->message->success('Usuario atualizado com sucesso')->flash();
-                    Helpers::redirect('admin/users/users');
-                }else{
-                    $user->message()->flash();
+                if ($user->save()) {
+                     $this->message->success('Usuário atualizado com sucesso')->flash();
+                      Helpers::redirect('admin/users/users');
+                } else {
+                    $user->mensagem()->flash();
                 }
             }
         }
 
-        echo $this->template->render('users/forms_users.html.twig',[
-            'user' => $user
+        echo $this->template->render('usuarios/formulario.html', [
+            'usuario' => $user
         ]);
     }
 
-    public function delete(int $id)
-    {
-        if(is_int($id)){
-            $user = (new UserModel())->findByID($id);
-            if(!$user){
-                $this->message->alert('O usuário que você esta tentando deletar não existe!')->flash();
-                Helpers::redirect('admin/users/users');
-            }else{
-                if($user->destroy()){
-                    $this->message->success('Usuário deletado com sucesso!')->flash();
-                    Helpers::redirect('admin/users/users');
-                }else{
-                    $this->message->error($user->error())->flash();
-                    Helpers::redirect('admin/users/users');
-                }
-            }
-        }
-    }
     
     /**
      * validateData
@@ -127,18 +116,46 @@ class AdminUser extends AdminController
      */
     public function validateData(array $data): bool
     {
-        if(empty($data['name'])){
-            $this->message->alert('Informe o nome do usuario')->flash();
+        if (empty($data['name'])) {
+            $this->message->alert('Informe o nome do usuário')->flash();
             return false;
         }
-        if(empty($data['email'])){
+        if (empty($data['email'])) {
             $this->message->alert('Informe o e-mail do usuário')->flash();
             return false;
         }
-        if(!Helpers::validateEmail($data['email'])){
-            $this->message->alert('Informe um e-mail válido')->flash();
+        if (!Helpers::validateEmail($data['email'])) {
+            $this->message->alert('Informe um e-mail válido!')->flash();
             return false;
         }
+
         return true;
     }
+
+    
+    /**
+     * delete
+     *
+     * @param  mixed $id
+     * @return void
+     */
+    public function delete(int $id): void
+    {
+        if (is_int($id)) {
+            $user = (new UserModel())->findByID($id);
+            if (!$user) {
+                $this->message->alert('O usuário que você está tentando deletar não existe!')->flash();
+                  Helpers::redirect('admin/users/users');
+            } else {
+                if ($user->destroy()) {
+                     $this->message->success('Usuário deletado com sucesso!')->flash();
+                      Helpers::redirect('admin/users/users');
+                } else {
+                    $this->message->error($user->erro())->flash();
+                      Helpers::redirect('admin/users/users');
+                }
+            }
+        }
+    }
+
 }
