c7297449efe4311099d96865ba49dce71989e1bf
Yuri do Monte <yspm.developer@gmail.com>
2024-06-18 18:39:06 -0300

    change admindashboard

--- system/Controller/Admin/AdminDashboard.php
+++ system/Controller/Admin/AdminDashboard.php
@@ -22,21 +22,26 @@ class AdminDashboard extends AdminController
        $categories = new CategoryModel();
        echo $this->template->render('dashboard.html.twig',[
         'posts' => [
-            'total'    => $post->find()->count(),
-            'active'   => $post->find('status = 1')->count(),
-            'inactive' => $post->find('status = 0')->count(),
+            'posts' => $post->find()->order('id DESC')->limit(5)->result(true),
+            'total'      => $post->find()->count(),
+            'active'     => $post->find('status = 1')->count(),
+            'inactive'   => $post->find('status = 0')->count(),
         ],
         'users' => [
-            'total'    => $user->find()->count(),
-            'active'   => $user->find('status = 1')->count(),
-            'inactive' => $user->find('status = 0')->count(),
+            'logins'         => $user->find()->order('last_login DESC')->limit(5)->result(true),
+            'users'          => $user->find('level != 3')->count(),
+            'users_active'   => $user->find('status = 1 AND level !=3')->count(),
+            'users_inactive' => $user->find('status = 0 AND level !=3')->count(),
+            'admin'          => $user->find('level = 3')->count(),
+            'admin_active'          => $user->find('status = 1 AND level = 3')->count(),
+            'admin_inactive' => $user->find('status = 0 AND level =3')->count()
         ],
-        // 'categories' => [
-        //     // 'total'    => $categories->find()->count(),
-        //     'active'   => $categories->find('status = ')->count(),
-        //     'inactive' => $categories->find('status = 0')->count()
+        'categories' => [
+            'total'    => $categories->find()->count(),
+            'active'   => $categories->find('status = 1')->count(),
+            'inactive' => $categories->find('status = 0')->count()
 
-        // ]
+        ]
        ]);
     }
     
