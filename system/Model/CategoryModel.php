b3a6e8c963142cf439dbeb9f1834508b5d8a65e8
Yuri do Monte <yspm.developer@gmail.com>
2024-06-18 22:29:47 -0300

    refactory categorimodel

--- system/Model/CategoryModel.php
+++ system/Model/CategoryModel.php
@@ -5,77 +5,12 @@ namespace system\Model;
 use PDOException;
 use system\Core\Connect;
 use system\Core\Message;
+use system\Core\Model;
 
-class CategoryModel
+class CategoryModel extends Model
 {
-    public function find(?string $terms = null): array
+    public function __construct()
     {
-        $terms = ($terms ? "WHERE {$terms}" : '');
-        
-        $query = "SELECT * FROM category {$terms} "; 
-        $stmt = Connect::getInstance()->query($query);
-        $result = $stmt->fetchAll();
-
-        return $result;        
+        parent::__construct('category');
     }
-    
-    public function findByID(int $id): bool|object
-    {
-        $query = "SELECT * FROM category WHERE id = {$id} "; 
-        $stmt = Connect::getInstance()->query($query);
-        $result = $stmt->fetch();
-
-        return $result; 
-    }
-    
-    public function posts(int $id): array
-    {
-        $query = "SELECT * FROM posts WHERE category_id = {$id} AND status = 1 ORDER BY id DESC "; 
-        $stmt = Connect::getInstance()->query($query);
-        $result = $stmt->fetchAll();
-
-        return $result;        
-    }
-    
-    public function register(array $data)
-    {
-       try {
-          $query = 'INSERT INTO category(`title`, `text`, `status`) VALUES (?,?,?)';
-          $stmt = Connect::getInstance()->prepare($query);
-          $stmt->execute(
-             [
-                $data['title'],
-                $data['text'],
-                $data['status']
-             ]);
-       } catch (PDOException $e) {
-          echo (new Message())->error($e);
-       }
-    }
-    
-    public function update(array $data, int $id):void
-    {
-        $query = "UPDATE category SET title = ?, text = ?, status = ? WHERE id = {$id} ";
-        $stmt = Connect::getInstance()->prepare($query);
-        $stmt->execute([$data['title'],$data['text'],$data['status']]);
-    }
-    
-     public function delete(int $id):void
-    {
-        $query = "DELETE FROM category WHERE id = {$id} ";
-        $stmt = Connect::getInstance()->prepare($query);
-        $stmt->execute();
-    }
-    
-    public function count(?string $terms = null): int
-    {
-        $terms = ($terms ? "WHERE {$terms}" : '');
-
-        $query = "SELECT * FROM category {$terms}";
-        $stmt = Connect::getInstance()->prepare($query);
-        $stmt->execute();
-
-        return $stmt->rowCount();
-    }
-    
 }
