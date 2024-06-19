5bffcea75e8e61e79493c5ac38dbf8f624cd245e
Yuri do Monte <yspm.developer@gmail.com>
2024-06-18 22:28:05 -0300

    refactore admincategories

--- system/Controller/Admin/AdminCategories.php
+++ system/Controller/Admin/AdminCategories.php
@@ -8,19 +8,19 @@ use system\Model\CategoryModel;
 
 class AdminCategories extends AdminController
 {
-    public function categories_list()
+    public function lists()
     {
         $categories = new CategoryModel();
 
         echo $this->template->render(
             'categories/categories.html.twig',
             [
-                'categories' => $categories->find(),
+                'categories' => $categories->find()->order('title ASC')->result(true),
                 'total'      => [
-                    'total'  => $categories->count(),
-                    'active'  => $categories->count('status = 1'),
-                    'inactive'  => $categories->count('status = 0')
-                ]    
+                    'total'  => $categories->find()->count(),
+                    'active'  => $categories->find('status = 1')->count(),
+                    'inactive'  => $categories->find('status = 0')->count()
+                ]
             ]
         );
     }
@@ -29,9 +29,21 @@ class AdminCategories extends AdminController
     {
         $data = filter_input_array(INPUT_POST, FILTER_DEFAULT);
         if (isset($data)) {
-            (new CategoryModel())->register($data);
-            $this->message->success('Categoria cadastrada com sucesso')->flash();
-            Helpers::redirect('admin/categories/categories');
+            if ($this->validateData($data)) {
+                $category = new CategoryModel();
+
+                $category->title = $data['title'];
+                $category->text  = $data['text'];
+                $category->status  = $data['status'];
+
+                if ($category->save()) {
+                    $this->message->success('Categoria cadastrada com sucesso')->flash();
+                    Helpers::redirect('admin/categories/categories');
+                } else {
+                    $this->message->error($category->error())->flash();
+                    Helpers::redirect('admin/categories/categories');
+                }
+            }
         }
 
         echo $this->template->render('categories/forms_categories.html.twig', []);
@@ -43,10 +55,20 @@ class AdminCategories extends AdminController
 
         $data = filter_input_array(INPUT_POST, FILTER_DEFAULT);
         if (isset($data)) {
-            (new CategoryModel())->update($data, $id);
-            
-            $this->message->success('Categoria atualizada com sucesso')->flash();
-            Helpers::redirect('admin/categories/categories');
+            if ($this->validateData($data)) {
+                $category->title = $data['title'];
+                $category->text  = $data['text'];
+                $category->status  = $data['status'];
+                $category->update_at = date('Y-m-d H:i:s');
+
+                if ($category->save()) {
+                    $this->message->success('Categoria cadastrada com sucesso')->flash();
+                    Helpers::redirect('admin/categories/categories');
+                } else {
+                    $this->message->error($category->error())->flash();
+                    Helpers::redirect('admin/categories/categories');
+                }
+            }
         }
 
         echo $this->template->render('categories/forms_categories.html.twig', [
@@ -56,9 +78,30 @@ class AdminCategories extends AdminController
 
     public function delete(int $id): void
     {
-        (new CategoryModel())->delete($id);
-        $this->message->success('Categoria deletada com sucesso')->flash();
-        Helpers::redirect('admin/categories/categories');
+        if (is_int($id)) {
+            $category = (new CategoryModel())->findByID($id);
+            if (!$category) {
+                $this->message->alert('A categoria que você esta tentando deletar não existe')->flash();
+                Helpers::redirect('admin/categories/categories');
+            } else {
+                if ($category->destroy()) {
+                    $this->message->success('Categoria deletada com sucesso!')->flash();
+                    Helpers::redirect('admin/categories/categories');
+                } else {
+                    $this->message->error($category->error())->flash();
+                    Helpers::redirect('admin/categories/categories');
+                }
+            }
+        }
     }
 
+    private function validateData(array $data): bool
+    {
+        if (empty($data['title'])) {
+            $this->message->alert('Escreva um título para o categoria!')->flash();
+            return false;
+        }
+
+        return true;
+    }
 }
