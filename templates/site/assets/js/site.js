6dedfb0be8ce1fefa6aa6ffbaf058df326ed88f7
Yuri do Monte <yspm.developer@gmail.com>
2024-06-20 08:28:03 -0300

    create function for wize textarea

--- templates/site/assets/js/site.js
+++ templates/site/assets/js/site.js
@@ -22,3 +22,20 @@ $(document).ready(function () {
         }
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const textareas = document.querySelectorAll('.autoResizeTextarea');
+
+    function autoResize(event) {
+        const textarea = event.target;
+        textarea.style.height = 'auto';
+        textarea.style.height = textarea.scrollHeight + 'px';
+    }
+
+    textareas.forEach(textarea => {
+        textarea.addEventListener('input', autoResize);
+        // Ajusta o tamanho inicial ao carregar a página se houver conteúdo pré-carregado
+        textarea.style.height = 'auto';
+        textarea.style.height = textarea.scrollHeight + 'px';
+    });
+});
