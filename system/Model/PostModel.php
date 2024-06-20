160b98038ced1c9503aedf3bffc8b65fd994a9a5
Yuri do Monte <yspm.developer@gmail.com>
2024-06-19 21:05:39 -0300

    new functions user and category

--- system/Model/PostModel.php
+++ system/Model/PostModel.php
@@ -11,5 +11,21 @@ class PostModel extends Model
   {
       parent:: __construct('posts');
   }
+
+  public function category(): ? CategoryModel
+  {
+    if($this->category_id){
+      return (new CategoryModel())->findByID($this->category_id);
+    }
+    return null;
+  }
+
+  public function user(): ? UserModel
+  {
+    if($this->user_id){
+      return (new UserModel())->findByID($this->user_id);
+    }
+    return null;
+  }
   
 }
