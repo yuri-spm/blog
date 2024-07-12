17ecbeba3ea9679f50befa6ce91588e1cb1e258b
Yuri do Monte <yspm.developer@gmail.com>
2024-07-11 23:41:23 -0300

    change config

--- system/config.php
+++ system/config.php
@@ -11,12 +11,12 @@ define('DB_NOME', 'blog');
 define('DB_USUARIO', 'root');
 define('DB_SENHA', '');
 
-//informações do sistema
-define('SITE_NOME', 'Helpx Support');
-define('SITE_DESCRICAO', 'Helpx - Tecnologia em Sistemas');
+//infoções do sistema
+define('SITE_NOME', 'Helpx');
+define('SITE_DESCRICAO', 'Helpx Support');
 
 //urls do sistema
-define('URL_PRODUCAO', 'https://helpx.com.br');
+define('URL_PRODUCAO', 'https://unset.com.br');
 define('URL_DESENVOLVIMENTO', 'http://localhost/blog');
 
 define('URL_SITE', 'blog/');
