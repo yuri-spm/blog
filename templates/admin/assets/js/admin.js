0ca98f1bf81eaaaad1c539abd3e914d33e768dce
Yuri do Monte <yspm.developer@gmail.com>
2024-07-13 23:20:00 -0300

    change language DataTable

--- templates/admin/assets/js/admin.js
+++ templates/admin/assets/js/admin.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
     $('.entity').DataTable({
         "pageLength": 5,
-        "lengthMenu": [5, 10, 25, 50, 75, 100]
+        "lengthMenu": [5, 10, 25, 50, 75, 100],
+        language: {
+            url: '//cdn.datatables.net/plug-ins/2.0.8/i18n/pt-BR.json',
+        }
     });
 });
