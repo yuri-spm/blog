ada2f8b491ad39ede2228ab29dac643bd33c282a
Yuri do Monte <yspm.developer@gmail.com>
2024-07-17 22:46:54 -0300

    change css and js

--- templates/admin/assets/js/admin.js
+++ templates/admin/assets/js/admin.js
@@ -1,26 +1,52 @@
 $(document).ready(function () {
-    var url = 'http://localhost/blog/admin'
-    $('#tabela').DataTable({
+    $.extend($.fn.dataTable.defaults, {
+        language: {
+            url: '//cdn.datatables.net/plug-ins/1.13.3/i18n/pt-BR.json'
+        }
+    });
+    var url = 'http://localhost/blog/';
+
+    $('#entityPost').DataTable({
         language: {
             url: '//cdn.datatables.net/plug-ins/1.13.3/i18n/pt-BR.json'
         },
         order: [[0, 'desc']],
         processing: true,
         serverSide: true,
-        ajax: 'http://localhost/blog/admin/posts/datatable',
-        columns:[
+        ajax: {
+            url: url + 'admin/posts/datatable',
+        },
+        columns: [
+            null,
+            {
+                data: null,
+                render: function (data, type, row) {
+                    if (row[1]) {
+                        return '<a data-fancybox data-caption="cover" class="overflow zoom" href="' + url + 'uploads/imagens/' + row[1] + '"><img src="' + url + 'uploads/imagens/thumbs/' + row[1] + '" class="cover-img" /></a>';
+                    } else {
+                        return '<i class="fa-regular fa-images fs-1 text-secondary"></i>';
+                    }
+                }
+            },
             null,
             null,
             null,
             {
                 data: null,
                 render: function (data, type, row) {
+                    if (row[5] === 1) {
+                        return '<i class="fa-solid fa-circle text-success" tooltip="tooltip" title="Ativo"></i>';
+                    } else {
+                        return '<i class="fa-solid fa-circle text-danger" tooltip="tooltip" title="Inativo"></i>';
+                    }
+                }
+            },
+            {
+                data: null,
+                render: function (data, type, row) {
                     var html = '';
-
-                    html += ' <a href=" ' + url + '/posts/editar/' + row[0] + ' " tooltip="tooltip" title="Editar"><i class="fa-solid fa-pen m-1"></i></a> ';
-
-                    html += '<a href=" ' + url + '/posts/deletar/' + row[0] + ' "><i class="fa-solid fa-trash m-1"></i></a>';
-
+                    html += ' <a href="' + url + 'admin/posts/edit/' + row[0] + '"><i class="fa-solid fa-pen m-1"></i></a> ';
+                    html += '<a href="' + url + 'admin/posts/delete/' + row[0] + '"><i class="fa-solid fa-trash m-1"></i></a>';
                     return html;
                 }
             }
