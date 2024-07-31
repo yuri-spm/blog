4c5e5545129d6cf24f4716e33bbb45ce113f91f6
Yuri do Monte <yspm.developer@gmail.com>
2024-07-31 12:22:37 -0300

    change js add jquery and validation

--- templates/site/assets/js/site.js
+++ templates/site/assets/js/site.js
@@ -1,27 +1,45 @@
 $(document).ready(function () {
   $("form").submit(function (event) {
-    var validate = false;
-    var name = $("#nome");
+    event.preventDefault();
 
-    if ($('#nome').val().trim() === "") {
-        $('#nome').addClass('is-invalid');
-        $('#nome').siblings('.invalid-feedback').html('Preencha o campo nome');
-        validate = true;
-    }else{
-        $('#nome').removeClass('is-invalid').addClass('is-valid');
-        $('#nome').siblings('.invalid-feedback').html('').removeClass('.invalid-feedback').addClass('.valid-feedback');
-      }
+    var loading = $('.ajaxLoading');
+    button = $(':input[type="submit"]');
 
-    if ($('#email').val().trim() === "") {
-        $('#email').addClass('is-invalid');
-        $('#email').siblings('.invalid-feedback').html('Preencha o campo email');
-        validate = true;
-    }else{
-        $('#email').removeClass('is-invalid').addClass('is-valid');
-        $('#email').siblings('.invalid-feedback').html('').removeClass('.invalid-feedback').addClass('.valid-feedback');
-      }
-    if (validate) {
-      event.preventDefault();
-    }
+    $.ajax({
+        type: 'POST',
+        url: $(this).attr('action'),
+        data: new FormData(this),
+        dataType: "json",
+        contentType: false,
+        processData: false,
+        beforeSend: function(){
+            loading.show().fadeIn(200);
+            button.prop('disable', false).addClass('disabled');
+        },
+        success: function (data) {
+            if(data.erro){
+               jBoxAlert(data.erro, 'yellow');
+            }
+            if(data.redirect){
+                window.location.href = data.redirect;
+            }
+        },
+        complete: function(){
+           loading.hide().fadeOut(200);
+           button.removeClass('disabled');
+        },
+        error:function(jqXHR, textSatus, errorThrown){
+            console.log(jqXHR, textSatus, errorThrown);
+        }
+    });
   });
 });
+
+function jBoxAlert(mensagem, cor) {
+    new jBox('Notice', {
+        content: mensagem,
+        color: cor,
+        animation: 'pulse',
+        showCountdown: true
+    });
+}
